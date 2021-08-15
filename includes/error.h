1ef75b156a31503ea291954686604dc34d3e2909
yuki-katayama <katayu810@gmail.com>
2021-08-15 18:56:46 +0900

    exitした時のエラー処理を追加しました。同時に、errorディレクトリ用のMakefileも追加

---
+++ includes/error.h
@@ -0,0 +1,8 @@
+#ifndef ERROR_H
+# define ERROR_H
+
+# include <unistd.h>
+
+void	numeric_argument_error(char *str);
+
+#endif
---
+++ srcs/error/error_exit.c
@@ -0,0 +1,9 @@
+#include "../../includes/error.h"
+#include "../../libft/libft.h"
+
+void	numeric_argument_error(char *str)
+{
+	ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+	ft_putstr_fd(str, STDERR_FILENO);
+	ft_putendl_fd(": numeric argument required", STDERR_FILENO);
+}
