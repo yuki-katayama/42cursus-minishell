29c3756b03a8acfcac925e367397162b0fb9ef84
yuki-katayama <katayu810@gmail.com>
2021-08-15 22:17:34 +0900

    add: libftの追加

---
+++ libft/ft_isdigit.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
--- libft/libft.h
+++ libft/libft.h
@@ -3,6 +3,7 @@
 
 # include <stdlib.h> //size_t malloc
 # include <unistd.h> //write
+# include <stdbool.h> // bool
 
 int		ft_malloc_p(void **p, size_t len);
 size_t	ft_strlen(const char *s);
@@ -12,6 +13,9 @@ int		ft_strcmp(const char *s1, const char *s2);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
+char	*ft_skip(const char *str, int len);
+int		ft_isdigit(int c);
+bool	ft_isdigitstr(char *str);
 
 
 #endif
