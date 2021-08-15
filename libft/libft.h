8338923382f2211f777c03f048e7e6f475bf91b8
yuki-katayama <katayu810@gmail.com>
2021-08-15 18:12:43 +0900

    add: fdで出力するために、必要なlibftを追加しました。

--- libft/libft.h
+++ libft/libft.h
@@ -2,11 +2,16 @@
 # define LIBFT_H
 
 # include <stdlib.h> //size_t malloc
+# include <unistd.h> //write
 
 int		ft_malloc_p(void **p, size_t len);
 size_t	ft_strlen(const char *s);
 char	*ft_charskip(const char *str, char skip);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
+
 
 #endif
