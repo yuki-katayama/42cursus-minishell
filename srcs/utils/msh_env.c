7f39cf4e25fde1738b2cf4fb0e19526d58d6f2b3
yuki-katayama <katayu810@gmail.com>
2021-09-20 18:00:36 +0900

    add: msh類

---
+++ srcs/utils/msh_env.c
@@ -0,0 +1,39 @@
+#include "../../includes/pipe.h"
+#include "../../libft/libft.h"
+
+char	*msh_get_env(char *key, t_env *env)
+{
+	if (!*key)
+		return ("$");
+	while (env)
+	{
+		if (!ft_strncmp(key, env->key, ft_strlen(env->key) + 1))
+			return (env->value);
+		env = env->next;
+	}
+	return ("");
+}
+
+t_env	*msh_del_env(char *key, t_env *env)
+{
+	t_env	*cur;
+	t_env	*prev;
+
+	prev = NULL;
+	cur = env;
+	while (cur)
+	{
+		if (ft_strncmp(key, cur->key, ft_strlen(key) + 1) == 0)
+		{
+			if (prev)
+				prev->next = cur->next;
+			else
+				env = cur->next;
+			cur = NULL;
+			break ;
+		}
+		prev = cur;
+		cur = cur->next;
+	}
+	return (env);
+}
