e05d123747253c663b0aeddef5b1817362633f6d
yuki-katayama <katayu810@gmail.com>
2022-01-10 21:54:12 +0900

    signal

--- srcs/signal/heredoc_signal.c
+++ srcs/signal/heredoc_signal.c
@@ -5,17 +5,22 @@
 static void	ft_heredoc_signal_sigint(int sig)
 {
 	(void)sig;
-	ft_put_str_fd("\b\b  \b\b\n", 1);
-	// rl_replace_line("", 0);
-	// rl_on_new_line();
-	// rl_redisplay();
+	ft_put_str_fd("\n", 1);
+	exit(1);
+}
+
+static void	ft_heredoc_signal_sigquit(int sig)
+{
+	(void)sig;
+	ft_put_str_fd("\b\b  \b\b", 1);
+	ft_put_str_fd("Quit: 3\n", 1);
 	exit(1);
 }
 
 void	ft_heredoc_sig_hundler(void)
 {
 	if (signal(SIGINT, ft_heredoc_signal_sigint) == SIG_ERR || \
-		signal(SIGQUIT, SIG_IGN) == SIG_ERR)
+		signal(SIGQUIT, ft_heredoc_signal_sigquit) == SIG_ERR)
 	{
 		ft_put_str_fd(strerror(errno), STDERR_FILENO);
 		exit(1);
